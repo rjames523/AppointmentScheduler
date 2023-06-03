051942765f8adf6bd27987d7436271b93324edf1
rjames523 <riley.james523@yahoo.com>
2023-06-03 17:05:01 -0400

    Adds globalization for the login form

---
+++ WindowsFormsApp1/schedulerLoginForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace schedulerLoginForm
+{
+    partial class schedulerLoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appNameLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTxtBox = new System.Windows.Forms.TextBox();
+            this.passwordTxtBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // appNameLabel
+            // 
+            this.appNameLabel.AutoSize = true;
+            this.appNameLabel.Font = new System.Drawing.Font("Goudy Old Style", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appNameLabel.Location = new System.Drawing.Point(139, 40);
+            this.appNameLabel.Name = "appNameLabel";
+            this.appNameLabel.Size = new System.Drawing.Size(194, 34);
+            this.appNameLabel.TabIndex = 0;
+            this.appNameLabel.Text = "The Scheduler";
+            this.appNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(150, 106);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(70, 16);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Username";
+            this.usernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(150, 175);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.passwordLabel.Size = new System.Drawing.Size(67, 16);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // usernameTxtBox
+            // 
+            this.usernameTxtBox.Location = new System.Drawing.Point(153, 125);
+            this.usernameTxtBox.Multiline = true;
+            this.usernameTxtBox.Name = "usernameTxtBox";
+            this.usernameTxtBox.Size = new System.Drawing.Size(170, 26);
+            this.usernameTxtBox.TabIndex = 3;
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.Location = new System.Drawing.Point(153, 194);
+            this.passwordTxtBox.Multiline = true;
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.Size = new System.Drawing.Size(170, 26);
+            this.passwordTxtBox.TabIndex = 4;
+            // 
+            // loginButton
+            // 
+            this.loginButton.AutoSize = true;
+            this.loginButton.Location = new System.Drawing.Point(190, 246);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(93, 34);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Copyright (c) 2023";
+            // 
+            // schedulerLoginForm
+            // 
+            this.AcceptButton = this.loginButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 328);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTxtBox);
+            this.Controls.Add(this.usernameTxtBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.appNameLabel);
+            this.Name = "schedulerLoginForm";
+            this.Text = "Appointment Scheduler";
+            this.Load += new System.EventHandler(this.schedulerLoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label appNameLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTxtBox;
+        private System.Windows.Forms.TextBox passwordTxtBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/schedulerLoginForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace schedulerLoginForm
+{
+    public partial class schedulerLoginForm : Form
+    {
+        public schedulerLoginForm()
+        {
+            InitializeComponent();
+        }
+
+        ResourceManager rm;
+        CultureInfo culture;
+        StreamWriter sw;
+
+        //public delegate void CultureChanged(object obj, EventArgs e);
+
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTxtBox.Text;
+            string password = passwordTxtBox.Text;
+            string timestampFormat = culture.DateTimeFormat.SortableDateTimePattern;
+
+            ///////////////////////////////////////////////////////////////////////
+            // Add check for incorrect/correct credentials (use database values)///
+            ///////////////////////////////////////////////////////////////////////
+
+            if (File.Exists("../login.log"))
+            {
+                var fileStream = new FileStream("../login.log", FileMode.Append, FileAccess.Write);
+                using (sw = new StreamWriter(fileStream))
+                {
+                    sw.WriteLine($"{DateTime.Now.ToString(timestampFormat)}   The user, {username}, has logged in successfully");
+                }
+            }
+            else
+            {
+                var fileStream = new FileStream("../login.log", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+                using (sw = new StreamWriter(fileStream))
+                {
+                    sw.WriteLine($"{DateTime.Now.ToString(timestampFormat)}   The user, {username}, has logged in successfully");
+                }
+            }
+        }
+
+        private void schedulerLoginForm_Load(object sender, EventArgs e)
+        {
+            rm = new ResourceManager("AppointmentScheduler.Resources.Res", typeof(schedulerLoginForm).Assembly);
+            culture = CultureInfo.CurrentUICulture;
+            usernameLabel.Text = rm.GetString("usernameLabel", culture);
+            passwordLabel.Text = rm.GetString("passwordLabel", culture);
+            loginButton.Text = rm.GetString("loginButton", culture);
+        }
+    }
+}
