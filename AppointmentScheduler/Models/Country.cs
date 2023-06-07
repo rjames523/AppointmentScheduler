015738c4e1a44b743047b7e3aeacad165943b658
rjames523 <riley.james523@yahoo.com>
2023-06-07 17:47:48 -0400

    Create Country.cs

---
+++ AppointmentScheduler/Models/Country.cs
@@ -0,0 +1,31 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Models
+{
+    public class Country
+    {
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public Timestamp LastUpdate { get; set; }
+        public string LastUpdatedBy { get; set; }
+
+        public Country() { }
+
+        public Country(int countryID, string countryName, DateTime createDate, string createdBy, Timestamp lastUpdate, string lastUpdatedBy)
+        {
+            CountryID = countryID;
+            CountryName = countryName;
+            CreateDate = createDate;
+            CreatedBy = createdBy;
+            LastUpdate = lastUpdate;
+            LastUpdatedBy = lastUpdatedBy;
+        }
+    }
+}
