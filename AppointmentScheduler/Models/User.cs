aedb811e81de5e896d54b7057d1a9f376734ef07
rjames523 <riley.james523@yahoo.com>
2023-06-07 17:47:52 -0400

    Create User.cs

---
+++ AppointmentScheduler/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool Active { get; set; } //Database value is likely either 0 or 1, aka False or True
+        public DateTime CreateDate { get; set; }
+        public User CreatedBy { get; set; } //Need to be string?
+        public Timestamp LastUpdate { get; set; }
+        public User LastUpdatedBy { get; set; } //Need to be string?
+
+        public User() { }
+
+        public User(int userID, string userName, string password, bool active, DateTime createDate, User createdBy, Timestamp lastUpdate, User lastUpdatedBy)
+        {
+            UserID = userID;
+            UserName = userName;
+            Password = password;
+            Active = active;
+            CreateDate = createDate;
+            CreatedBy = createdBy;
+            LastUpdate = lastUpdate;
+            LastUpdatedBy = lastUpdatedBy;
+        }
+    }
+}
