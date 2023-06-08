8eec122b42f79784aebef5f91dd7196857e9bce8
rjames523 <riley.james523@yahoo.com>
2023-06-08 18:42:08 -0400

    Added method for authentication that compares input values with database values

--- AppointmentScheduler/Connections/DBConnection.cs
+++ AppointmentScheduler/Connections/DBConnection.cs
@@ -9,17 +9,17 @@ using MySqlX.XDevAPI.Relational;
 
 namespace AppointmentScheduler.Connections
 {
-    internal class DBConnection
+    public class DBConnection
     {
         MySqlConnection connection;
         MySqlCommand command;
         MySqlDataReader reader;
         // home laptop testing -- string connectionStr = @"server=localhost;userid=testuser;password=Lancaster123!;database=client_schedule";
-        string connectionStr = @"server=localhost;userid=admin;password=Lancaster123!;database=client_schedule"; // work laptop testing
+        private string _connectionStr = @"server=localhost;userid=admin;password=Lancaster123!;database=client_schedule"; // work laptop testing
 
         public DBConnection()
         {
-            connection = new MySqlConnection(connectionStr);
+            connection = new MySqlConnection(_connectionStr);
         }
 
         public void OpenConnection()
@@ -32,7 +32,27 @@ namespace AppointmentScheduler.Connections
             connection.Close();
         }
 
-        public void GetAllCustomers() // change to return list
+        public bool AuthenticateUser(string username, string password)
+        {
+            connection.Open();
+
+            string sql = $"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'";
+            command = new MySqlCommand(sql, connection);
+            reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                connection.Close();
+                return true;
+            }
+            else
+            {
+                connection.Close();
+                return false;
+            }
+        }
+
+        public List<Customer> GetAllCustomers() // change to return list
         {
             connection.Open();
 
@@ -43,9 +63,9 @@ namespace AppointmentScheduler.Connections
 
             while (reader.Read())
             {
+                Customer newCust = new Customer();
                 for (int i = 0; i < reader.FieldCount; i++)
                 {
-                    Customer newCust = new Customer();
                     switch (reader.GetName(i).ToLower())
                     {
                         case "customerid":
@@ -56,6 +76,7 @@ namespace AppointmentScheduler.Connections
                             break;
                         case "addressid":
                             //get the address ID ***
+                            newCust.AddressID = (int)reader.GetValue(i);
                             break;
                         case "active":
                             newCust.Active = (bool)reader.GetValue(i);
@@ -73,17 +94,21 @@ namespace AppointmentScheduler.Connections
                             newCust.LastUpdate = (DateTime)reader.GetValue(i);
                             break;
                             // do something
-                        case "lastupdatedby":
-                            newCust.LastUpdatedBy = (string)reader.GetValue(i);
+                        case "lastupdateby":
+                            newCust.LastUpdateBy = (string)reader.GetValue(i);
                             break;
                             // do something
                         default:
-                            return;
+                            return null;
                     }
                 }
+
+                customerList.Add(newCust);
             }
 
             connection.Close();
+
+            return customerList;
 
         }
     }
