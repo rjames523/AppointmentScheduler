555387fb75a12047c40909474ad05bbd3484d7f4
rjames523 <riley.james523@yahoo.com>
2023-06-13 17:23:02 -0400

    Update Appointment.cs

--- AppointmentScheduler/Models/Appointment.cs
+++ AppointmentScheduler/Models/Appointment.cs
@@ -24,7 +24,7 @@ namespace AppointmentScheduler.Models
         public DateTime CreateDate { get; set; }
         public string CreatedBy { get; set; }
         public DateTime LastUpdate { get; set; }
-        public string LastUpdateBy { get; set; }
+        public string LastUpdatedBy { get; set; }
 
 
         public Appointment() { }
@@ -50,7 +50,7 @@ namespace AppointmentScheduler.Models
             CreateDate = createDate;
             CreatedBy = createdBy;
             LastUpdate = lastUpdate;
-            LastUpdateBy = LastUpdateBy;
+            LastUpdatedBy = LastUpdateBy;
         }
     }
 }
