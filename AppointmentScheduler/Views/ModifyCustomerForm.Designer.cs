2efa59490d33b9a9c45d5959844a488adb521c33
rjames523 <riley.james523@yahoo.com>
2023-06-19 17:13:41 -0400

    Updated the modify customers form to allow customer information modification

--- AppointmentScheduler/Views/ModifyCustomerForm.Designer.cs
+++ AppointmentScheduler/Views/ModifyCustomerForm.Designer.cs
@@ -28,19 +28,250 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cityComboBox = new System.Windows.Forms.ComboBox();
+            this.address2TextBox = new System.Windows.Forms.TextBox();
+            this.address2Label = new System.Windows.Forms.Label();
+            this.updateCustomerButton = new System.Windows.Forms.Button();
+            this.appNameLabel = new System.Windows.Forms.Label();
+            this.postalCodeTxtBox = new System.Windows.Forms.TextBox();
+            this.postalCodeLabel = new System.Windows.Forms.Label();
+            this.custNameTxtBox = new System.Windows.Forms.TextBox();
+            this.streetAddrTxtBox = new System.Windows.Forms.TextBox();
+            this.phoneTxtBox = new System.Windows.Forms.TextBox();
+            this.countryComboBox = new System.Windows.Forms.ComboBox();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.countryLabel = new System.Windows.Forms.Label();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.customerNameLabel = new System.Windows.Forms.Label();
+            this.activeCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // cityComboBox
+            // 
+            this.cityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cityComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityComboBox.FormattingEnabled = true;
+            this.cityComboBox.Location = new System.Drawing.Point(72, 311);
+            this.cityComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.cityComboBox.Name = "cityComboBox";
+            this.cityComboBox.Size = new System.Drawing.Size(192, 26);
+            this.cityComboBox.TabIndex = 28;
+            // 
+            // address2TextBox
+            // 
+            this.address2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address2TextBox.Location = new System.Drawing.Point(73, 258);
+            this.address2TextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.address2TextBox.Name = "address2TextBox";
+            this.address2TextBox.Size = new System.Drawing.Size(405, 24);
+            this.address2TextBox.TabIndex = 26;
+            // 
+            // address2Label
+            // 
+            this.address2Label.AutoSize = true;
+            this.address2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address2Label.Location = new System.Drawing.Point(70, 236);
+            this.address2Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.address2Label.Name = "address2Label";
+            this.address2Label.Size = new System.Drawing.Size(147, 18);
+            this.address2Label.TabIndex = 25;
+            this.address2Label.Text = "Address 2 (Optional):";
+            // 
+            // updateCustomerButton
+            // 
+            this.updateCustomerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateCustomerButton.Location = new System.Drawing.Point(223, 434);
+            this.updateCustomerButton.Margin = new System.Windows.Forms.Padding(4);
+            this.updateCustomerButton.Name = "updateCustomerButton";
+            this.updateCustomerButton.Size = new System.Drawing.Size(103, 47);
+            this.updateCustomerButton.TabIndex = 35;
+            this.updateCustomerButton.Text = "Update";
+            this.updateCustomerButton.UseVisualStyleBackColor = true;
+            this.updateCustomerButton.Click += new System.EventHandler(this.updateCustomerButton_Click);
+            // 
+            // appNameLabel
+            // 
+            this.appNameLabel.AutoSize = true;
+            this.appNameLabel.Font = new System.Drawing.Font("Goudy Old Style", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appNameLabel.Location = new System.Drawing.Point(173, 47);
+            this.appNameLabel.Name = "appNameLabel";
+            this.appNameLabel.Size = new System.Drawing.Size(194, 34);
+            this.appNameLabel.TabIndex = 18;
+            this.appNameLabel.Text = "The Scheduler";
+            this.appNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // postalCodeTxtBox
+            // 
+            this.postalCodeTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postalCodeTxtBox.Location = new System.Drawing.Point(73, 377);
+            this.postalCodeTxtBox.Margin = new System.Windows.Forms.Padding(4);
+            this.postalCodeTxtBox.Name = "postalCodeTxtBox";
+            this.postalCodeTxtBox.Size = new System.Drawing.Size(192, 24);
+            this.postalCodeTxtBox.TabIndex = 32;
+            // 
+            // postalCodeLabel
+            // 
+            this.postalCodeLabel.AutoSize = true;
+            this.postalCodeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postalCodeLabel.Location = new System.Drawing.Point(69, 357);
+            this.postalCodeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.postalCodeLabel.Name = "postalCodeLabel";
+            this.postalCodeLabel.Size = new System.Drawing.Size(94, 18);
+            this.postalCodeLabel.TabIndex = 31;
+            this.postalCodeLabel.Text = "Postal Code:";
+            // 
+            // custNameTxtBox
+            // 
+            this.custNameTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.custNameTxtBox.Location = new System.Drawing.Point(73, 146);
+            this.custNameTxtBox.Margin = new System.Windows.Forms.Padding(4);
+            this.custNameTxtBox.Name = "custNameTxtBox";
+            this.custNameTxtBox.Size = new System.Drawing.Size(405, 24);
+            this.custNameTxtBox.TabIndex = 20;
+            // 
+            // streetAddrTxtBox
+            // 
+            this.streetAddrTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.streetAddrTxtBox.Location = new System.Drawing.Point(73, 203);
+            this.streetAddrTxtBox.Margin = new System.Windows.Forms.Padding(4);
+            this.streetAddrTxtBox.Name = "streetAddrTxtBox";
+            this.streetAddrTxtBox.Size = new System.Drawing.Size(405, 24);
+            this.streetAddrTxtBox.TabIndex = 24;
+            // 
+            // phoneTxtBox
+            // 
+            this.phoneTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneTxtBox.Location = new System.Drawing.Point(287, 377);
+            this.phoneTxtBox.Margin = new System.Windows.Forms.Padding(4);
+            this.phoneTxtBox.Name = "phoneTxtBox";
+            this.phoneTxtBox.Size = new System.Drawing.Size(192, 24);
+            this.phoneTxtBox.TabIndex = 34;
+            // 
+            // countryComboBox
+            // 
+            this.countryComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.countryComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countryComboBox.FormattingEnabled = true;
+            this.countryComboBox.Location = new System.Drawing.Point(287, 311);
+            this.countryComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.countryComboBox.Name = "countryComboBox";
+            this.countryComboBox.Size = new System.Drawing.Size(192, 26);
+            this.countryComboBox.TabIndex = 30;
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneLabel.Location = new System.Drawing.Point(283, 357);
+            this.phoneLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(55, 18);
+            this.phoneLabel.TabIndex = 33;
+            this.phoneLabel.Text = "Phone:";
+            // 
+            // countryLabel
+            // 
+            this.countryLabel.AutoSize = true;
+            this.countryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countryLabel.Location = new System.Drawing.Point(283, 292);
+            this.countryLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.countryLabel.Name = "countryLabel";
+            this.countryLabel.Size = new System.Drawing.Size(64, 18);
+            this.countryLabel.TabIndex = 29;
+            this.countryLabel.Text = "Country:";
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityLabel.Location = new System.Drawing.Point(69, 292);
+            this.cityLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(37, 18);
+            this.cityLabel.TabIndex = 27;
+            this.cityLabel.Text = "City:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressLabel.Location = new System.Drawing.Point(69, 183);
+            this.addressLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(109, 18);
+            this.addressLabel.TabIndex = 23;
+            this.addressLabel.Text = "Street Address:";
+            // 
+            // customerNameLabel
+            // 
+            this.customerNameLabel.AutoSize = true;
+            this.customerNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerNameLabel.Location = new System.Drawing.Point(69, 127);
+            this.customerNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.customerNameLabel.Name = "customerNameLabel";
+            this.customerNameLabel.Size = new System.Drawing.Size(122, 18);
+            this.customerNameLabel.TabIndex = 19;
+            this.customerNameLabel.Text = "Customer Name:";
+            // 
+            // activeCheckBox
+            // 
+            this.activeCheckBox.Location = new System.Drawing.Point(401, 104);
+            this.activeCheckBox.Name = "activeCheckBox";
+            this.activeCheckBox.Size = new System.Drawing.Size(78, 24);
+            this.activeCheckBox.TabIndex = 36;
+            this.activeCheckBox.Text = "Active?";
+            this.activeCheckBox.UseVisualStyleBackColor = true;
             // 
             // ModifyCustomerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(537, 373);
+            this.ClientSize = new System.Drawing.Size(549, 516);
+            this.Controls.Add(this.activeCheckBox);
+            this.Controls.Add(this.cityComboBox);
+            this.Controls.Add(this.address2TextBox);
+            this.Controls.Add(this.address2Label);
+            this.Controls.Add(this.updateCustomerButton);
+            this.Controls.Add(this.appNameLabel);
+            this.Controls.Add(this.postalCodeTxtBox);
+            this.Controls.Add(this.postalCodeLabel);
+            this.Controls.Add(this.custNameTxtBox);
+            this.Controls.Add(this.streetAddrTxtBox);
+            this.Controls.Add(this.phoneTxtBox);
+            this.Controls.Add(this.countryComboBox);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.countryLabel);
+            this.Controls.Add(this.cityLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.customerNameLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ModifyCustomerForm";
             this.Text = "Modify Customer";
+            this.Load += new System.EventHandler(this.ModifyCustomerForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cityComboBox;
+        private System.Windows.Forms.TextBox address2TextBox;
+        private System.Windows.Forms.Label address2Label;
+        private System.Windows.Forms.Button updateCustomerButton;
+        private System.Windows.Forms.Label appNameLabel;
+        private System.Windows.Forms.TextBox postalCodeTxtBox;
+        private System.Windows.Forms.Label postalCodeLabel;
+        private System.Windows.Forms.TextBox custNameTxtBox;
+        private System.Windows.Forms.TextBox streetAddrTxtBox;
+        private System.Windows.Forms.TextBox phoneTxtBox;
+        private System.Windows.Forms.ComboBox countryComboBox;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label countryLabel;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label customerNameLabel;
+        private System.Windows.Forms.CheckBox activeCheckBox;
     }
 }
--- AppointmentScheduler/Views/ModifyCustomerForm.cs
+++ AppointmentScheduler/Views/ModifyCustomerForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppointmentScheduler.Connections;
+using AppointmentScheduler.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,107 @@ namespace AppointmentScheduler
 {
     public partial class ModifyCustomerForm : Form
     {
+        public Customer selectedCustomer;
+        private DbConn conn;
+
         public ModifyCustomerForm()
         {
             InitializeComponent();
+        }
+
+        public ModifyCustomerForm(Customer selectedCustomer)
+        {
+            this.selectedCustomer = selectedCustomer;
+            InitializeComponent();
+        }
+
+        private void updateCustomerButton_Click(object sender, EventArgs e)
+        {
+            // Checks to see if any textbox on the form is null or whitespace
+            if (this.Controls.OfType<TextBox>().Any(x => string.IsNullOrWhiteSpace(x.Text) && x.Name != "address2TextBox") && !string.IsNullOrWhiteSpace(countryComboBox.Text) && !string.IsNullOrWhiteSpace(cityComboBox.Text))
+            {
+                BuildErrorMessage();
+            }
+            else
+            {
+                selectedCustomer.CustomerName = custNameTxtBox.Text.Trim();
+                selectedCustomer.Address.Address1 = streetAddrTxtBox.Text.Trim();
+                selectedCustomer.Address.Address2 = address2TextBox.Text.Trim();
+                selectedCustomer.Address.PostalCode = postalCodeTxtBox.Text.Trim();
+                selectedCustomer.Address.Phone = phoneTxtBox.Text.Trim();
+                selectedCustomer.LastUpdate = DateTime.Now.ToUniversalTime();
+                selectedCustomer.LastUpdatedBy = DbConn.loggedInUser.UserName;
+                selectedCustomer.Active = activeCheckBox.Checked;
+
+                selectedCustomer.Address.City.Country.CountryID = countryComboBox.SelectedIndex + 1;
+                selectedCustomer.Address.City.CityID = cityComboBox.SelectedIndex + 1;
+
+
+                conn.UpdateCustomerCountry(selectedCustomer.Address.City.Country.CountryID, countryComboBox.SelectedItem.ToString());
+                conn.UpdateCustomerCity(selectedCustomer.Address.City.CityID, selectedCustomer.Address.City.Country.CountryID, cityComboBox.Text);
+                conn.UpdateAddress(selectedCustomer);
+
+                conn.UpdateCustomer(selectedCustomer);
+
+                MessageBox.Show("Customer information has been updated successfully.", "The Scheduler - Modify Customer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Close();
+            }
+        }
+
+        private void ModifyCustomerForm_Load(object sender, EventArgs e)
+        {
+            conn = new DbConn();
+            countryComboBox.DataSource = conn.GetAllCountries().Select(x => x.CountryName).ToList();
+            cityComboBox.DataSource = conn.GetAllCities().Select(x => x.CityName).ToList();
+
+            custNameTxtBox.Text = selectedCustomer.CustomerName;
+            streetAddrTxtBox.Text = selectedCustomer.Address.Address1;
+            address2TextBox.Text = selectedCustomer.Address.Address2;
+            postalCodeTxtBox.Text = selectedCustomer.Address.PostalCode;
+            phoneTxtBox.Text = selectedCustomer.Address.Phone;
+            activeCheckBox.Checked = selectedCustomer.Active;
+
+            // Set the selected item in the city and country combo boxes based on the customer's current city and country
+            countryComboBox.SelectedIndex = conn.GetAllCountries().Where(y => selectedCustomer.Address.City.Country.CountryID == y.CountryID).Select(y => y.CountryID - 1).First();
+            cityComboBox.SelectedIndex = conn.GetAllCities().Where(y => selectedCustomer.Address.City.CityID == y.CityID).Select(y => y.CityID - 1).First();
+
+            this.ActiveControl = custNameTxtBox;
+        }
+
+        private void BuildErrorMessage()
+        {
+            // Creates a StringBuilder object for the error message
+            StringBuilder errorBlankInfo = new StringBuilder();
+
+            errorBlankInfo.Append("The following fields are required to add customer:\n\n");
+
+            if (string.IsNullOrWhiteSpace(custNameTxtBox.Text))
+            {
+                errorBlankInfo.AppendLine("Customer Name");
+            }
+            if (string.IsNullOrWhiteSpace(streetAddrTxtBox.Text))
+            {
+                errorBlankInfo.AppendLine("Street Address");
+            }
+            if (string.IsNullOrWhiteSpace(cityComboBox.Text))
+            {
+                errorBlankInfo.AppendLine("City");
+            }
+            if (string.IsNullOrWhiteSpace(countryComboBox.Text))
+            {
+                errorBlankInfo.AppendLine("Country");
+            }
+            if (string.IsNullOrWhiteSpace(postalCodeTxtBox.Text))
+            {
+                errorBlankInfo.AppendLine("Postal Code");
+            }
+            if (string.IsNullOrWhiteSpace(phoneTxtBox.Text))
+            {
+                errorBlankInfo.AppendLine("Phone");
+            }
+
+            MessageBox.Show(errorBlankInfo.ToString(), "The Scheduler", MessageBoxButtons.OK, MessageBoxIcon.Hand);
         }
     }
 }
