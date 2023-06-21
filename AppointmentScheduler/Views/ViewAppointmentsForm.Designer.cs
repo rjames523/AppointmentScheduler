bee9669120ed8af573d876e5019e1ca9eaf13167
rjames523 <riley.james523@yahoo.com>
2023-06-21 13:35:26 -0400

    Updated

---
+++ AppointmentScheduler/Views/ViewAppointmentsForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace AppointmentScheduler
+{
+    partial class ViewAppointmentsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customersDGV = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectedCustNameTxtBox = new System.Windows.Forms.TextBox();
+            this.appNameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.custApptsListBox = new System.Windows.Forms.ListBox();
+            this.editApptButton = new System.Windows.Forms.Button();
+            this.cancelApptButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customersDGV
+            // 
+            this.customersDGV.AllowUserToAddRows = false;
+            this.customersDGV.AllowUserToDeleteRows = false;
+            this.customersDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.customersDGV.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.customersDGV.Location = new System.Drawing.Point(13, 117);
+            this.customersDGV.Margin = new System.Windows.Forms.Padding(4);
+            this.customersDGV.MultiSelect = false;
+            this.customersDGV.Name = "customersDGV";
+            this.customersDGV.RowHeadersVisible = false;
+            this.customersDGV.RowHeadersWidth = 51;
+            this.customersDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.customersDGV.Size = new System.Drawing.Size(576, 297);
+            this.customersDGV.TabIndex = 1;
+            this.customersDGV.SelectionChanged += new System.EventHandler(this.customersDGV_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(692, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Viewing appointments for:";
+            // 
+            // selectedCustNameTxtBox
+            // 
+            this.selectedCustNameTxtBox.Location = new System.Drawing.Point(695, 162);
+            this.selectedCustNameTxtBox.Multiline = true;
+            this.selectedCustNameTxtBox.Name = "selectedCustNameTxtBox";
+            this.selectedCustNameTxtBox.Size = new System.Drawing.Size(311, 30);
+            this.selectedCustNameTxtBox.TabIndex = 19;
+            // 
+            // appNameLabel
+            // 
+            this.appNameLabel.AutoSize = true;
+            this.appNameLabel.Font = new System.Drawing.Font("Goudy Old Style", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appNameLabel.Location = new System.Drawing.Point(470, 36);
+            this.appNameLabel.Name = "appNameLabel";
+            this.appNameLabel.Size = new System.Drawing.Size(194, 34);
+            this.appNameLabel.TabIndex = 20;
+            this.appNameLabel.Text = "The Scheduler";
+            this.appNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Customers:";
+            // 
+            // custApptsListBox
+            // 
+            this.custApptsListBox.FormattingEnabled = true;
+            this.custApptsListBox.ItemHeight = 16;
+            this.custApptsListBox.Location = new System.Drawing.Point(695, 199);
+            this.custApptsListBox.Name = "custApptsListBox";
+            this.custApptsListBox.Size = new System.Drawing.Size(407, 212);
+            this.custApptsListBox.TabIndex = 22;
+            // 
+            // editApptButton
+            // 
+            this.editApptButton.Location = new System.Drawing.Point(744, 428);
+            this.editApptButton.Name = "editApptButton";
+            this.editApptButton.Size = new System.Drawing.Size(75, 32);
+            this.editApptButton.TabIndex = 23;
+            this.editApptButton.Text = "Edit";
+            this.editApptButton.UseVisualStyleBackColor = true;
+            this.editApptButton.Click += new System.EventHandler(this.editApptButton_Click);
+            // 
+            // cancelApptButton
+            // 
+            this.cancelApptButton.Location = new System.Drawing.Point(863, 428);
+            this.cancelApptButton.Name = "cancelApptButton";
+            this.cancelApptButton.Size = new System.Drawing.Size(75, 32);
+            this.cancelApptButton.TabIndex = 24;
+            this.cancelApptButton.Text = "Cancel";
+            this.cancelApptButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewAppointmentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 491);
+            this.Controls.Add(this.cancelApptButton);
+            this.Controls.Add(this.editApptButton);
+            this.Controls.Add(this.custApptsListBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.appNameLabel);
+            this.Controls.Add(this.selectedCustNameTxtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.customersDGV);
+            this.Name = "ViewAppointmentsForm";
+            this.Text = "The Scheduler - Appointments";
+            this.Load += new System.EventHandler(this.ViewAppointmentsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.customersDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView customersDGV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox selectedCustNameTxtBox;
+        private System.Windows.Forms.Label appNameLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox custApptsListBox;
+        private System.Windows.Forms.Button editApptButton;
+        private System.Windows.Forms.Button cancelApptButton;
+    }
+}
---
+++ AppointmentScheduler/Views/ViewAppointmentsForm.cs
@@ -0,0 +1,110 @@
+﻿using AppointmentScheduler.Connections;
+using AppointmentScheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppointmentScheduler
+{
+    public partial class ViewAppointmentsForm : Form
+    {
+        public ViewAppointmentsForm()
+        {
+            InitializeComponent();
+        }
+
+        DbConn conn;
+        private bool formLoaded;
+
+        Appointment selectedCustAppt;
+
+        private void ViewAppointmentsForm_Load(object sender, EventArgs e)
+        {
+            formLoaded = false;
+            conn = new DbConn();
+            conn.GetAllCustomers();
+            conn.GetAllCustomerAppointments();
+            customersDGV.DataSource = Appointment.AllCustomerAppts;
+            customersDGV.ClearSelection();
+            formLoaded = true;
+        }
+
+        private void customersDGV_SelectionChanged(object sender, EventArgs e)
+        {
+            if (formLoaded)
+            {
+                Customer customer = GetSelectedCustomer();
+
+                if (customer != null)
+                {
+                    selectedCustNameTxtBox.Text = customer.CustomerName;
+
+                    List<Appointment> selectedCustomerAppts = GetCustomerSpecificAppts(customer);
+
+                    if (Appointment.AllCustomerAppts.Count == 0)
+                    {
+                        custApptsListBox.Items.Add("There are no scheduled appointments.");
+                    }
+                    else
+                    {
+                        foreach (Appointment appt in selectedCustomerAppts)
+                        {
+                            custApptsListBox.Items.Add($"{appt.Type}\t{appt.Start.ToShortTimeString()}\t{appt.End.ToShortTimeString()}");
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+
+                // Stops this block from running again when selection is changed
+                formLoaded = false;
+            }
+
+            if (custApptsListBox.SelectedIndex > -1)
+            {
+                selectedCustAppt = Appointment.AllCustomerAppts.Where(x => x.AppointmentID == custApptsListBox.SelectedIndex + 1).First();
+            }
+        }
+
+        private List<Appointment> GetCustomerSpecificAppts(Customer customer)
+        {
+            var custAppts = Appointment.AllCustomerAppts.Where(x => x.CustomerID == customer.CustomerID).ToList();
+            return custAppts;
+        }
+
+        public Customer GetSelectedCustomer()
+        {
+            Customer selectedCustomer = new Customer();
+
+            foreach (DataGridViewRow row in customersDGV.SelectedRows)
+            {
+                foreach (DataGridViewColumn col in customersDGV.Columns)
+                {
+                    if (col.Name == "CustomerID")
+                    {
+                        selectedCustomer = (Customer)Customer.AllCustomers.Where(x => x.CustomerID == (int)row.Cells[col.Index].Value).Select(x => x).First();
+                    }
+                }
+            }
+
+            return selectedCustomer;
+        }
+
+        private void editApptButton_Click(object sender, EventArgs e)
+        {
+            ModifyApptsForm modifyApptForm = new ModifyApptsForm(selectedCustAppt);
+            Hide();
+            modifyApptForm.ShowDialog();
+            this.Show();
+        }
+    }
+}
