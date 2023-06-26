32181e6587e97fb331ff4d6a01139d5f5bafd7c1
rjames523 <riley.james523@yahoo.com>
2023-06-26 18:43:40 -0400

    Update SchedulerLoginForm.cs

--- AppointmentScheduler/Views/SchedulerLoginForm.cs
+++ AppointmentScheduler/Views/SchedulerLoginForm.cs
@@ -23,6 +23,8 @@ namespace schedulerLoginForm
         public SchedulerLoginForm()
         {
             InitializeComponent();
+            CultureInfo.CurrentCulture.ClearCachedData();
+            CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture;
         }
 
         ResourceManager rm;
