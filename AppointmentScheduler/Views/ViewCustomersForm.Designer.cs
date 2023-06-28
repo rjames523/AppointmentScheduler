64264737985b53bd682942634002d7193c5d0714
rjames523 <riley.james523@yahoo.com>
2023-06-28 18:16:28 -0400

    Update ViewCustomersForm.Designer.cs

--- AppointmentScheduler/Views/ViewCustomersForm.Designer.cs
+++ AppointmentScheduler/Views/ViewCustomersForm.Designer.cs
@@ -33,6 +33,7 @@
             this.scheduleAppointmentButton = new System.Windows.Forms.Button();
             this.deleteCustomerButton = new System.Windows.Forms.Button();
             this.editCustomerButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.customersDGV)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,25 +41,24 @@
             // 
             this.customersDGV.AllowUserToAddRows = false;
             this.customersDGV.AllowUserToDeleteRows = false;
+            this.customersDGV.AllowUserToResizeRows = false;
             this.customersDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.customersDGV.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.customersDGV.Location = new System.Drawing.Point(17, 16);
-            this.customersDGV.Margin = new System.Windows.Forms.Padding(4);
+            this.customersDGV.Location = new System.Drawing.Point(13, 13);
             this.customersDGV.MultiSelect = false;
             this.customersDGV.Name = "customersDGV";
             this.customersDGV.RowHeadersVisible = false;
             this.customersDGV.RowHeadersWidth = 51;
             this.customersDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.customersDGV.Size = new System.Drawing.Size(948, 439);
+            this.customersDGV.Size = new System.Drawing.Size(711, 357);
             this.customersDGV.TabIndex = 0;
             this.customersDGV.SelectionChanged += new System.EventHandler(this.customersDGV_SelectionChanged);
             // 
             // addCustomerButton
             // 
-            this.addCustomerButton.Location = new System.Drawing.Point(76, 486);
-            this.addCustomerButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addCustomerButton.Location = new System.Drawing.Point(35, 404);
             this.addCustomerButton.Name = "addCustomerButton";
-            this.addCustomerButton.Size = new System.Drawing.Size(131, 55);
+            this.addCustomerButton.Size = new System.Drawing.Size(98, 45);
             this.addCustomerButton.TabIndex = 1;
             this.addCustomerButton.Text = "Add Customer";
             this.addCustomerButton.UseVisualStyleBackColor = true;
@@ -66,10 +66,9 @@
             // 
             // scheduleAppointmentButton
             // 
-            this.scheduleAppointmentButton.Location = new System.Drawing.Point(543, 486);
-            this.scheduleAppointmentButton.Margin = new System.Windows.Forms.Padding(4);
+            this.scheduleAppointmentButton.Location = new System.Drawing.Point(319, 404);
             this.scheduleAppointmentButton.Name = "scheduleAppointmentButton";
-            this.scheduleAppointmentButton.Size = new System.Drawing.Size(131, 55);
+            this.scheduleAppointmentButton.Size = new System.Drawing.Size(98, 45);
             this.scheduleAppointmentButton.TabIndex = 2;
             this.scheduleAppointmentButton.Text = "Schedule Appointment";
             this.scheduleAppointmentButton.UseVisualStyleBackColor = true;
@@ -77,10 +76,9 @@
             // 
             // deleteCustomerButton
             // 
-            this.deleteCustomerButton.Location = new System.Drawing.Point(776, 486);
-            this.deleteCustomerButton.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteCustomerButton.Location = new System.Drawing.Point(461, 404);
             this.deleteCustomerButton.Name = "deleteCustomerButton";
-            this.deleteCustomerButton.Size = new System.Drawing.Size(131, 55);
+            this.deleteCustomerButton.Size = new System.Drawing.Size(98, 45);
             this.deleteCustomerButton.TabIndex = 3;
             this.deleteCustomerButton.Text = "Delete Customer";
             this.deleteCustomerButton.UseVisualStyleBackColor = true;
@@ -88,26 +86,36 @@
             // 
             // editCustomerButton
             // 
-            this.editCustomerButton.Location = new System.Drawing.Point(309, 486);
-            this.editCustomerButton.Margin = new System.Windows.Forms.Padding(4);
+            this.editCustomerButton.Location = new System.Drawing.Point(177, 404);
             this.editCustomerButton.Name = "editCustomerButton";
-            this.editCustomerButton.Size = new System.Drawing.Size(131, 55);
+            this.editCustomerButton.Size = new System.Drawing.Size(98, 45);
             this.editCustomerButton.TabIndex = 4;
             this.editCustomerButton.Text = "Edit Customer";
             this.editCustomerButton.UseVisualStyleBackColor = true;
             this.editCustomerButton.Click += new System.EventHandler(this.editCustomerButton_Click);
             // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(603, 404);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(98, 45);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
             // ViewCustomersForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(981, 567);
+            this.ClientSize = new System.Drawing.Size(736, 489);
+            this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.editCustomerButton);
             this.Controls.Add(this.deleteCustomerButton);
             this.Controls.Add(this.scheduleAppointmentButton);
             this.Controls.Add(this.addCustomerButton);
             this.Controls.Add(this.customersDGV);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ViewCustomersForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "The Scheduler - Customers";
@@ -124,5 +132,6 @@
         private System.Windows.Forms.Button scheduleAppointmentButton;
         private System.Windows.Forms.Button deleteCustomerButton;
         private System.Windows.Forms.Button editCustomerButton;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
